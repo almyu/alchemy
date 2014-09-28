caa3de7a005d15ebb293e20862abd8d6820372cb
almyu <hire@indeed.li>
2014-09-29 03:04:43 +0800

    Add delayed sprite script, restore scene

---
+++ Assets/Alchemy/Scripts/DelayedSprite.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DelayedSprite : MonoBehaviour {
+
+    public float delay = 1.0f;
+
+    private void Awake() {
+        GetComponent<SpriteRenderer>().enabled = false;
+        Invoke("Show", delay);
+    }
+
+    private void Show() {
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
