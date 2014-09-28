df4555ad7985671a7178f73527f8b9a1fa865dc5
almyu <hire@indeed.li>
2014-09-29 03:07:33 +0800

    Cleanup nested particle systems

--- Assets/Alchemy/Scripts/ParticleCleanup.cs
+++ Assets/Alchemy/Scripts/ParticleCleanup.cs
@@ -4,6 +4,7 @@ public class ParticleCleanup : MonoBehaviour {
 
     public void Cleanup() {
         foreach (var ps in FindObjectsOfType<ParticleSystem>())
-            Destroy(ps.gameObject);
+            if (ps)
+                Destroy(ps.gameObject);
     }
 }
