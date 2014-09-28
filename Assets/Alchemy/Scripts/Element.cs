6327d317126368795b0be96daf78aeb4d4228823
almyu <hire@indeed.li>
2014-09-28 23:23:07 +0800

    Add simple dragging

---
+++ Assets/Alchemy/Scripts/Element.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Element : MonoBehaviour {
+
+    private bool picked;
+    private Vector2 pickOffset;
+
+
+    public static Vector2 GetWorldCursor() {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDown() {
+        picked = true;
+        pickOffset = (Vector2) transform.position - GetWorldCursor();
+    }
+
+    private void OnMouseUp() {
+        picked = false;
+    }
+
+    private void Update() {
+        if (!picked) return;
+
+        transform.position = GetWorldCursor() + pickOffset;
+    }
+}
